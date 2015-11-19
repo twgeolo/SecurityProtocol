4eb8c6bad82c81db0ccf0eb4dafc47147e0e9217
George Lo <twgeolo@gmail.com>
2015-11-18 20:18:47 -0500

    Finished UI

--- server.java
+++ server.java
@@ -11,13 +11,15 @@ import javax.swing.event.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.net.*;
+import javax.swing.text.*;
 
 public class server {
-	
+
 	static JScrollPane history;
-	static JLabel chatLabel;
+	static JTextPane chatLabel;
 	static HistoryEntry[] entries;
-	
+	static JList<HistoryEntry> list;
+
 	public static void setupUI() {
     	JFrame mainFrame = new JFrame("Server UI");
 
@@ -30,17 +32,19 @@ public class server {
     	historyBar.setBackground(Color.white);
     	historyBar.setOpaque(true);
 		historyBar.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.gray));
-		
+
 		// History List
 
-		entries = new HistoryEntry[] {};
-    	JList<HistoryEntry> list = new JList<HistoryEntry>(entries);
+		entries = new HistoryEntry[] {
+			new HistoryEntry("Let's begin", "Past.png")
+		};
+    	list = new JList<HistoryEntry>(entries);
 		font = new Font("Helvetica Neue", Font.PLAIN, 15);
     	list.setFont(font);
     	list.setCellRenderer(new HistoryCellRenderer());
     	history = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		history.setBorder(BorderFactory.createEmptyBorder());
-		
+
 		// Main
 
     	JLabel mainBar = new JLabel("Main", JLabel.CENTER);
@@ -50,63 +54,76 @@ public class server {
     	mainBar.setBackground(Color.white);
     	mainBar.setOpaque(true);
 		mainBar.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 0, Color.gray));
-		
+
 		// Victor image
-		
+
 		ImagePanel victorPanel = new ImagePanel("Victor.png");
 		victorPanel.setBackground(Color.white);
-		
+
 		// Victor label
-		
+
 		JLabel victorLabel = new JLabel("Victor", JLabel.CENTER);
 		font = new Font("Helvetica Neue", Font.BOLD, 20);
 		victorLabel.setFont(font);
 		victorLabel.setForeground(Color.black);
 		victorLabel.setBackground(Color.white);
 		victorLabel.setOpaque(true);
-		
+
 		// Victor chat box
-		
-		chatLabel = new JLabel("Let's begin", JLabel.CENTER);
+
+		chatLabel = new JTextPane();
 		font = new Font("Helvetica Neue", Font.PLAIN, 15);
 		chatLabel.setFont(font);
+		chatLabel.setText("Let's begin");
 		chatLabel.setForeground(Color.black);
 		chatLabel.setBackground(Color.white);
 		chatLabel.setBorder(new RoundedBorder(Color.BLACK, 20));
 		chatLabel.setOpaque(true);
-		
+		StyledDocument doc = chatLabel.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+
 		// Set the bounds
-		
-    	historyBar.setBounds(0, 0, 240, 33);
-    	history.setBounds(0, 33, 240, 522);
-    	mainBar.setBounds(240, 0, 528, 33);
-		victorPanel.setBounds(454, 288, 100, 100);
-		victorLabel.setBounds(454, 388, 100, 30);
-		chatLabel.setBounds(354, 138, 300, 100);
-		
+
+    	historyBar.setBounds(0, 0, 320, 33);
+    	history.setBounds(0, 33, 320, 522);
+    	mainBar.setBounds(320, 0, 448, 33);
+		victorPanel.setBounds(494, 288, 100, 100);
+		victorLabel.setBounds(494, 388, 100, 30);
+		chatLabel.setBounds(369, 163, 350, 40);
+
 		// Add the subviews
-		
+
 		mainFrame.getContentPane().add(historyBar);
 		mainFrame.getContentPane().add(history);
 		mainFrame.getContentPane().add(mainBar);
 		mainFrame.getContentPane().add(victorPanel);
 		mainFrame.getContentPane().add(victorLabel);
 		mainFrame.getContentPane().add(chatLabel);
-		
+
 		mainFrame.getContentPane().setBackground(Color.white);
     	mainFrame.setResizable(false);
     	mainFrame.setSize(768,576);
     	mainFrame.setLayout(null);
     	mainFrame.setVisible(true);
-
-//		list.setListData( newdata );
-//      	contact.revalidate();
-//      	contact.repaint();
 	}
-	
+
+	public static void setTextToLabel(String str) {
+		String listText = "<html>" + str.replaceAll(": ", "<br>") + "</html>";
+		int length = entries.length;
+    	HistoryEntry[] array = Arrays.copyOf(entries, length + 1);
+    	array[length] = new HistoryEntry(listText, "Past.png");
+		entries = array;
+		list.setListData(entries);
+     	history.revalidate();
+     	history.repaint();
+		chatLabel.setText(str);
+	}
+
     public static void main (String[] args) {
 		setupUI();
-		
+
         int runtimes = (int)(Math.random()*11+5);  //5 to 15 times
         int flag = 0, flag2 = 0;
         try {
@@ -124,7 +141,7 @@ public class server {
                          System.out.println();
                          }
                          System.out.println();*/
-                        
+
                         ObjectInputStream input2 = new ObjectInputStream(socket.getInputStream());
                         String[][] g2matrix = (String[][])input2.readObject();
                         /*for(int counter = 0; counter < g2matrix.length; counter++) {
@@ -134,7 +151,7 @@ public class server {
                          System.out.println();
                          }
                          System.out.println();*/
-                        
+
                         /*ObjectInputStream input3 = new ObjectInputStream(socket.getInputStream());
                          String[][] array3 = (String[][])input3.readObject();*/
                         /*for(int counter = 0; counter < array3.length; counter++) {
@@ -151,7 +168,7 @@ public class server {
                             int value = (int)(Math.random()*2);
                             writer.write(String.valueOf(value));
                             writer.flush();
-							chatLabel.setText("Run #" + runnum + ": Waiting for commitment");
+							setTextToLabel("Run #" + runnum + ": Waiting for commitment");
                             System.out.println("Waiting for commitment");
                             ObjectInputStream inputmG3 = new ObjectInputStream(socket.getInputStream());
                             String[][] modifiedG3 = (String[][])inputmG3.readObject();
@@ -162,10 +179,10 @@ public class server {
                                 System.out.println();
                             }*/
                             if(value == 0) {
-								chatLabel.setText("Run #" + runnum + ": Requesting for alpha and Q");
+								setTextToLabel("Run #" + runnum + ": Requesting for alpha and Q");
                                 System.out.println("Requesting for alpha and Q");
                             } else if(value == 1) {
-								chatLabel.setText("Run #" + runnum + ": Requesting for pi and subgraph Q'");
+								setTextToLabel("Run #" + runnum + ": Requesting for pi and subgraph Q'");
                                 System.out.println("Requesting for pi and subgraph Q'");
                             }
                             writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -196,9 +213,11 @@ public class server {
             }
         }
         catch (IOException e) {
+			setTextToLabel(e.toString());
             System.out.println(e);
         }
         catch (ClassNotFoundException e) {
+			setTextToLabel(e.toString());
             System.out.println(e);
         }
     }
